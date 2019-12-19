8eb285555cf66e7a598bd1cc90f6128048c3d677
pranjaldatta <pranjaldatta99@gmail.com>
2019-12-19 17:41:29 +0530

    conda env yaml+sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+conda env create -f env.yaml
+eval "$(conda shell.bash hook)"
+conda activate test
+TARGDIR="$CONDA_PREFIX/lib/python3.7/site-packages"
+BASEDIR=$(readlink -f "$0")
+BASEDIR=$(dirname $BASEDIR)
+cp -r $BASEDIR $TARGDIR

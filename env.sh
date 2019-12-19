2bdf88af7004f85819dc6b2d5b74cd5b9e175418
Pranjal  Datta <pranjaldatta99@gmail.com>
2019-12-20 04:08:41 +0530

    changed test to facenet

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 conda env create -f env.yaml
 eval "$(conda shell.bash hook)"
-conda activate test
+conda activate facenet
 TARGDIR="$CONDA_PREFIX/lib/python3.7/site-packages"
 BASEDIR=$(readlink -f "$0")
 BASEDIR=$(dirname $BASEDIR)
